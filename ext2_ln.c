bff275f4354501b2ef2af127cd9de663dc263a34
Gilberto <gilberto03@live.ca>
2014-12-02 11:35:13 -0500

    Update ext2_ln.c

---
+++ ext2_ln.c
@@ -0,0 +1,5 @@
+ext2_ln: This program takes three command line arguments. The first is the name of an ext2 formatted virtual disk. 
+The other two are absolute paths on your ext2 formatted disk. The program should work like ln, creating a link 
+from the first specified file to the second specified path. If the first location does not exist, if the second 
+location already exists, or if the first location refers to a directory, then your program should return the 
+appropriate error. 
