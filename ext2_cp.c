13b3135845a90c9c35f2b4c928831d8f49f753b6
Gilberto <gilberto.aristizabal@mail.utoronto.ca>
2014-12-02 13:09:39 -0500

    working with cp

--- ext2_cp.c
+++ ext2_cp.c
@@ -5,3 +5,33 @@ formatted disk. The program should work like cp, copying the file on your native
 location on the disk. If the specified file or target location does not exist, then your program should 
 return the appropriate error. 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SBLOCATION
+#define INODEMAP
+#define DATAMAP
+
+struct superBlock{
+	char *content;
+};
+
+int main (int argc, char *argv[]){
+
+	if(argc != 2){
+		printf("invalid number of arguments \n");
+	
+
+	} else {
+		FILE *file = fopen(argv[1], "r");
+		if(file == 0){
+			printf("Could not open file %s \n", argv[1]);
+		
+		} else {
+			struct superBlock sb = malloc(sizeof(struct superBlock));
+			fread(sb, SBLOCATION, 1, file);
+			printf("$s \n", sb->content);
+		}
+	}	
+}
