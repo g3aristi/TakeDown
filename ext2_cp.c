2e91ff56ad1860fe040102efa084242123face2d
Gilberto <gilberto03@live.ca>
2014-12-02 11:34:22 -0500

    Update ext2_cp.c

---
+++ ext2_cp.c
@@ -0,0 +1,5 @@
+ext2_cp: This program takes three command line arguments. The first is the name of an ext2 formatted virtual disk. 
+The second is the path to a file on your native operating system, and the third is an absolute path on your ext2 
+formatted disk. The program should work like cp, copying the file on your native file system onto the specified 
+location on the disk. If the specified file or target location does not exist, then your program should 
+return the appropriate error. 
