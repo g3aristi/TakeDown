f9a52485afadfe966ff4a98d1c7f64a09e809f4f
Gilberto <gilberto.aristizabal@mail.utoronto.ca>
2014-12-02 11:43:06 -0500

    starting programs

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -1,4 +1,6 @@
+/*
 ext2_mkdir: This program takes two command line arguments. The first is the name of an ext2 formatted virtual disk. 
 The second is an absolute path on your ext2 formatted disk. The program should work like mkdir, creating the final 
 directory on the specified path on the disk. If location where the final directory is to be created does 
 not exist or if the specified directory already exists, then your program should return the appropriate error.
+*/
